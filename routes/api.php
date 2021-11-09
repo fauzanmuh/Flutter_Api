bbde6853b691b748a31b09bfd02fbc03e78175fd
fauzanmuh <f4uz4n.oioi@gmail.com>
2021-11-09 17:42:10 +0700

    Add Transaction

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('categories', CategoryController::class);
+
+Route::apiResource('transactions', TransactionController::class);
