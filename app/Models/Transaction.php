91fb0027ecbd8215e2d4fa8b1857a09df6159e04
fauzanmuh <f4uz4n.oioi@gmail.com>
2021-11-11 14:39:03 +0700

    GlobalScope to filter data by user id

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -31,5 +32,14 @@ class Transaction extends Model
     public function setTransactionAttribute($value) {
         $this->attributes['transaction_date'] = Carbon::createFromFormat('m/d/Y', $value)
         ->format('Y-m-d');
+    }
+
+    protected static function booted()
+    {
+        if(auth()->check()) {
+            static::addGlobalScope('by_user', function (Builder $builder) {
+                $builder->where('user_id', auth()->id());
+            });
+        }
     }
 }
